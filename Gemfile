a1f4f6f4fec3683c207669950d3651ceb1e14007
Joanna092 <kochanowska92@gmail.com>
2021-09-15 20:58:41 +0200

    changed photo storage from public to AWS

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.7.1'
 
+gem 'aws-sdk', '~> 3.0'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
 # Use Puma as the app server
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,7 +30,10 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :local
+  #config.active_storage.service = :local
+
+  # Using the Amazon S3 settings for Active Storage
+  config.active_storage.service = :amazon
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
